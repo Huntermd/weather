ee225f62122c826b820635c6cae0b71948279a40
Hunter Davis <huntermd111@gmail.com>
2023-03-01 13:37:08 -0800

    Working with data in Weather Summary

--- components/WeatherSummary.js
+++ components/WeatherSummary.js
@@ -2,7 +2,17 @@ import { StyleSheet, Text, View, Image } from 'react-native'
 import React from 'react'
 import heroImage from '../assets/Sunny.jpg'
 import {SizeConstants, ColorConstants, FontConstants} from '../GlobalStyles';
+import data from '../data';
+
+
 export default function WeatherSummary() {
+
+let windDirection;
+let deg= data.current.wind_deg;
+
+if(deg >(360-22.5) || deg < 22.5) windDirection = 'North';
+if(deg >22.5 && deg < 45) windDirection = 'North E';
+
   return (
     <View style = {styles.heroContainer}>
       <Text style= {styles.text}>Good Afternoon</Text>
@@ -10,7 +20,14 @@ export default function WeatherSummary() {
       <View style = {styles.TempContainer}>
         <Text style = {styles.highTemp}>46{'\u00b0'}f</Text>
         <Text style = {styles.lowTemp}>/33 {'\u00b0'}f</Text>
+        
       </View>
+      <View>
+        <Text style={[styles.SumText, {fontSize: 30}]}>Huntington</Text>
+        <Text style={styles.SumText}> Today - {data.daily[0].weather[0].main} with a high near {Math.round(data.daily[0].temp.max)}.
+        Southwest wind around {Math.round(data.daily[0].wind_speed)} mph, with gust as high as {Math.round(data.daily[0].wind_gust)} mph.
+        </Text>
+        </View>
     </View>
     
   )
@@ -55,5 +72,8 @@ const styles = StyleSheet.create({
         aspectRatio: 1,
         borderRadius: 20,
         resizeMode: 'cover'
+      },
+      SumText:{
+        color: 'white',
       }
 })
