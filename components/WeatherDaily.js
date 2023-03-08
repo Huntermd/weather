87438abc905ceb40df319350153035a2042bb9de
Hunter Davis <huntermd111@gmail.com>
2023-03-07 18:33:35 -0800

    created the container style

--- components/WeatherDaily.js
+++ components/WeatherDaily.js
@@ -1,18 +1,22 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import { Day } from './Day';
+import  Day  from './Day';
 import data from '../data';
 
 export default function WeatherDaily() {
   return (
-    <View>
-        <Day day={data.daily[0]}/>
-      <Day day={data.daily[1]}/>
-      <Day day={data.daily[2]}/>
-      <Day day={data.daily[3]}/>
-      <Day day={data.daily[4]}/>
+    <View style={styles.Container}>
+        <Day  day={data.daily[0]}/>
+      <Day  day={data.daily[1]} />
+      <Day   day={data.daily[2]}/>
+      <Day  day={data.daily[3]} />
+      <Day  day={data.daily[4]} />
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  Container:{
+    flexDirection: 'row'
+  }
+})
